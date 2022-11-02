e0f1df0338708b554eb76cdb13dfe01b6aa3a320
Shuky25 <vojinsundovic@gmail.com>
2022-11-02 09:18:30 +0100

    update

--- datoteke/vezba1/Form1.cs
+++ datoteke/vezba1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,45 @@ namespace vezba1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        
+        
+        private void tbDodaj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Osoba o = new Osoba(tbIme.Text, tbPrezime.Text, Convert.ToInt32(tbGodine.Text));
+                StreamWriter sw = new StreamWriter("spisak.txt", true);
+                o.Pisi(sw);
+                sw.Close();
+                tbIme.Clear();
+                tbIme.Focus();
+                tbPrezime.Clear();
+                tbGodine.Clear();
+            }catch(Exception)
+            {
+                MessageBox.Show("GRESKA");
+            }
+        }
+
+        private void tbIspis_Click(object sender, EventArgs e)
+        {
+            Osoba o, max;
+            lbIspis.Items.Clear();
+            StreamReader sr = new StreamReader("spisak.txt");
+            max = new Osoba();
+            max.Citaj(sr);
+            lbIspis.Items.Add(max.ToString());
+            while (!sr.EndOfStream)
+            {
+                o = new Osoba();
+                o.Citaj(sr);
+                lbIspis.Items.Add(o.ToString());
+                if (o.StarijiOd(max)) max = o;
+            }
+            textBox1.Text = max.ToString();
+            sr.Close();
         }
     }
 }
---
+++ datoteke/vezba1/Osoba.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vezba1
+{
+    internal class Osoba
+    {
+        string ime, prezime;
+        int godine;
+
+        public Osoba(string ime, string prezime, int godine)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.godine = godine;
+        }
+
+        public Osoba()
+        {
+        }
+
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+
+        public string Prezime
+        {
+            get { return prezime; }
+            set { prezime = value; }
+        }
+
+        public int Godine
+        {
+            get { return godine; }
+            set { godine = value; }
+        }
+
+        public void Citaj(StreamReader sr)
+        {
+            Ime = sr.ReadLine();
+            Prezime = sr.ReadLine();
+            Godine = Convert.ToInt32(sr.ReadLine());
+        }
+
+        public void Pisi(StreamWriter sw)
+        {
+            sw.WriteLine(Ime);
+            sw.WriteLine(Prezime);
+            sw.WriteLine(Godine.ToString());
+        }
+
+        public bool StarijiOd(Osoba o)
+        {
+            return this.Godine > o.Godine;
+        }
+        public bool MladjiOd(Osoba o)
+        {
+            return this.Godine < o.Godine;
+        }
+        public bool Jednake(Osoba o)
+        {
+            return this.Godine == o.Godine;
+        }
+
+        public override string ToString()
+        {
+            return Ime + " " + Prezime + " " + Godine;
+        }
+    }
+}
